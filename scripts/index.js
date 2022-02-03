df0cb6cd14ff6077b53bedef0ec6318402becdda
Abderahim ABBAS <abderahim.abbas@gmail.com>
2022-02-03 19:51:37 +0100

    first version

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,49 @@
 'use strict';
-
+/* var API = chrome || browser; */
 console.log('Hola 👋');
+
+const lpar2rrdServerUrl = 'https://demo.lpar2rrd.com';
+const virtualMachineList = document.getElementById('list');
+const fileName = 'lpar-config.csv';
+
+// Download CSV file
+
+console.log(lpar2rrdServerUrl + '/' + fileName);
+
+function onStartedDownload(id) {
+  console.log(`Started downloading: ${id}`);
+}
+
+function onFailed(error) {
+  console.log(`Download failed !!!: ${error}`);
+}
+
+let myHeaders = new Headers();
+myHeaders.append('method', 'GET');
+myHeaders.append('credentials', 'include');
+
+const donwloadFile = async () => {
+  try {
+    console.log('Trying download file');
+    const response = await fetch(lpar2rrdServerUrl + '/' + fileName, {
+      mode: 'no-cors',
+      credentials: 'include',
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'text/csv',
+    });
+    if (response.ok) {
+      onStartedDownload(fileName);
+    } else {
+      throw response.status;
+    }
+  } catch (error) {
+    onFailed(error);
+  }
+};
+
+// Select button
+
+const buttonToDownload = document.querySelector('#download');
+buttonToDownload.addEventListener('click', donwloadFile);
+
+//
